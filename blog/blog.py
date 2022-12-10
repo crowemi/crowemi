e61cdb157610c5752c2debe30f4d0223501a7fde
crowemi <crowemi@hotmail.com>
2022-12-10 14:34:15 -0800

    added foundations for cron; added blog

--- blog/blog.py
+++ blog/blog.py
@@ -1,8 +1,26 @@
 import os
+
 from flask import Blueprint, render_template
+from markdown import markdown
+
+from helper.aws import get_object
 
 blog = Blueprint("blog", __name__, subdomain="blog")
 
 @blog.route("/", subdomain='blog')
 def index():
-    return "Hello World! From Blog."
+    try:
+        html = markdown(get_object('crowemi', f'Obsidian/blog/about.md'))
+    except Exception as e:
+        print(e)
+        html = "Uh oh! Something isn't right. Please try again later."
+    return render_template("blog.html", page_content=html)
+
+@blog.route("/investing", subdomain="blog")
+def investing(id: str = None):
+    try:
+        html = markdown(get_object('crowemi', f'Obsidian/blog/investing/about.md'))
+    except Exception as e:
+        print(e)
+        html = "Uh oh! Something isn't right. Please try again later."
+    return render_template("blog.html", page_content=html)
---
+++ tests/test_aws.py
@@ -0,0 +1,4 @@
+from helper.aws import get_object
+
+def test_get_object():
+    ret = get_object()
