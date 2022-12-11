376841a9f3b47a53e0b8995856f62f8fb99277bf
crowemi <crowemi@hotmail.com>
2022-12-11 15:18:47 -0800

    added logging; added pip  dependencies

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
-import logging
+from logging.config import dictConfig
 
 from flask import Flask
 
 from crowemi.crowemi import crowemi
 from blog.blog import blog
 
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "default",
+            },
+            "file": {
+                "class": "logging.FileHandler",
+                "filename": "flask.log",
+                "formatter": "default",
+            },
+        },
+        "root": {"level": "DEBUG", "handlers": ["console", "file"]},
+    }
+)
+
 app = Flask(__name__, subdomain_matching=True)
-app.config["SERVER_NAME"] = 'crowemi.com' #TODO: find a better way to config 'crowemi.local:5000'
+
+app.config["SERVER_NAME"] = 'localhost:8080' #TODO: find a better way to config 'crowemi.local:5000'
 
 app.register_blueprint(crowemi)
 app.register_blueprint(blog, subdomain="blog")
 
 
 if __name__ == "__main__":
-    app.config["SERVER_NAME"] = 'crowemi.com' #TODO: find a better way to config 'crowemi.local:5000'
+    app.config["SERVER_NAME"] = 'localhost:8080' #TODO: find a better way to config 'crowemi.local:5000'
     app.run(debug=True)
 
