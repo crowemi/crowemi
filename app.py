502243949580b94f558865fc464664296f381e74
crowemi <crowemi@hotmail.com>
2023-01-07 13:40:31 -0800

    started updating blog layout; added dynamic aws links

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from logging.config import dictConfig
 
 from flask import Flask
@@ -30,21 +31,16 @@ dictConfig(
     }
 )
 
-SERVER_NAME = {
-    "development": "crowemi.local:5000",
-    "docker": "localhost:8080",
-    "production": "crowemi.com"
-}
 
 app = Flask(__name__, subdomain_matching=True)
 
-app.config["SERVER_NAME"] = 'crowemi.com' #TODO: find a better way to config 'crowemi.local:5000'
+app.config["SERVER_NAME"] = os.getenv("server_name")
 
 app.register_blueprint(crowemi)
 app.register_blueprint(blog, subdomain="blog")
 
 
 if __name__ == "__main__":
-    app.config["SERVER_NAME"] = 'crowemi.com' #TODO: find a better way to config 'crowemi.local:5000'
+    app.config["SERVER_NAME"] = os.getenv("server_name")
     app.run(debug=True)
 
