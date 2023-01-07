2e51ef4d7ce7457075e65788ac3455d13a670722
crowemi <crowemi@hotmail.com>
2023-01-07 14:14:43 -0800

    updated with aws secrets; added dynamic page loading

--- blog/blog.py
+++ blog/blog.py
@@ -7,6 +7,10 @@ from helper.aws import *
 
 blog = Blueprint("blog", __name__, subdomain="blog")
 
+BUCKET = "crowemi"
+# TODO: determine what happens when there is no default page, we should check if the document exists and the
+# then display a default page if not.
+DEFAULT_PAGE = 'about'
 BASE_ROUTE = "Obsidian/blog/"
 
 
@@ -19,16 +23,17 @@ def index():
         html = "Uh oh! Something isn't right. Please try again later."
     return render_template("blog.html", page_content=html)
 
-@blog.route("/investing/", subdomain = "blog")
-@blog.route("/investing/<id>", subdomain="blog")
-def investing(id: str = None):
+@blog.route("/<subject>/", subdomain = "blog")
+@blog.route("/<subject>/<id>", subdomain="blog")
+def investing(subject: str = None, id: str = None):
     try:
-        if id:
-            pass
-        else:
-            id = 'about'
-        html = markdown(get_object_content('crowemi', f'{BASE_ROUTE}investing/{id}.md'))
+        if not id:
+            id = DEFAULT_PAGE
+        key = f'{BASE_ROUTE}{subject}/{id}.md'
+        page_content = markdown(get_object_content(BUCKET, key))
+        document = get_object(BUCKET, key)
+        last_modified = document['LastModified']
     except Exception as e:
         print(e)
-        html = "Uh oh! Something isn't right. Please try again later."
-    return render_template("blog.html", page_content=html)
+        page_content = "Uh oh! Something isn't right. Please try again later."
+    return render_template("blog.html", page_content=page_content, last_modified=last_modified)
