1e51aef73a8cf3f9659d899e39b56e1ef15cab0b
crowemi <crowemi@hotmail.com>
2023-01-30 05:31:31 -0800

    major updates to blog

--- blog/blog.py
+++ blog/blog.py
@@ -12,25 +12,35 @@ BUCKET = "crowemi"
 # then display a default page if not.
 DEFAULT_PAGE = 'about'
 BASE_ROUTE = "Obsidian/blog/"
+SUBJECTS = [
+    { "name": 'investing', "paths" : list() },
+    { "name": 'finance', "paths" : list() },
+    { "name": 'fitness', "paths" : list() },
+                                          
+]
 
 
 @blog.route("/", subdomain='blog')
 def index():
     try:
+        get_paths()
         html = markdown(get_object_content('crowemi', f'Obsidian/blog/about.md'))
     except Exception as e:
         print(e)
         html = "Uh oh! Something isn't right. Please try again later."
-    return render_template("blog.html", page_content=html)
+    return render_template("blog.html", page_content=html, subjects=SUBJECTS)
 
 @blog.route("/<subject>/", subdomain = "blog")
 @blog.route("/<subject>/<id>", subdomain="blog")
 def page(subject: str = None, id: str = None):
     try:
+        get_paths()
         last_modified = None
         if not id:
             id = DEFAULT_PAGE
-        key = f'{BASE_ROUTE}{subject}/{id}.md'
+            key = f'{BASE_ROUTE}{subject}/{id}.md'
+        else:
+            key = f"{BASE_ROUTE}{subject}/{base64.b64decode(id).decode('utf-8')}"
         if object_exists(BUCKET, key):
             page_content = markdown(get_object_content(BUCKET, key))
             document = get_object(BUCKET, key)
@@ -40,4 +50,12 @@ def page(subject: str = None, id: str = None):
     except Exception as e:
         print(e)
         page_content = "Uh oh! Something isn't right. Please try again later."
-    return render_template("blog.html", page_content=page_content, last_modified=last_modified)
+    return render_template("blog.html", page_content=page_content, last_modified=last_modified, subjects=SUBJECTS)
+
+def get_paths():
+    for subject in SUBJECTS:
+        objects = list_objects(f"{BASE_ROUTE}{subject['name']}/", BUCKET)
+        prefix = objects['Prefix']
+        keys = objects['Contents']
+        paths = parse_path(keys, prefix)
+        subject['paths'] = paths
--- helper/aws.py
+++ helper/aws.py
@@ -1,3 +1,4 @@
+import base64
 import boto3
 
 resource = boto3.resource('s3')
@@ -24,3 +25,33 @@ def list_objects(prefix: str, bucket: str, next_token: str = None):
 def object_exists(bucket: str, key: str):
     ret = client.list_objects_v2(Bucket=bucket, Prefix=key)
     return True if ret['KeyCount'] > 0 else False
+
+def parse_path(keys: list, prefix: str) -> object:
+    paths = {}
+
+    for key in keys:
+        k = prefix.count('/')
+        post_prefix = key['Key'].split(prefix)[1]
+        post_prefix_paths = post_prefix.split("/")
+        # traverse file structure
+        i = 0
+        current_position = paths
+        while i < len(post_prefix_paths):
+            current_path = post_prefix_paths[i]
+            if len(current_path.split('.')) > 1:
+                # page
+                if "root" in current_position:
+                    current_position["root"].append([current_path, base64.b64encode(bytes(post_prefix, 'utf-8'))])
+                else:
+                    root = list()
+                    root.append([current_path, base64.b64encode(bytes(post_prefix, 'utf-8'))])
+                    current_position["root"] = root
+            else:
+                if current_path in current_position:
+                    current_position = current_position[current_path]
+                else:
+                    current_position[current_path] = {}
+                    current_position = current_position[current_path]
+            i += 1
+
+    return paths
