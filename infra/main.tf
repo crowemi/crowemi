c549b5e60e79af2f8d3c7ef60e4d361c75d0377c
Andy Crowe <crowemi@hotmail.com>
2026-02-16 22:16:09 -0800

    remove unnecssary

--- infra/main.tf
+++ infra/main.tf
@@ -3,31 +3,31 @@ locals {
     service = "crowemi"
     project = var.gcp_project_id
 }
-resource "google_service_account" "service_account" {
+resource "google_service_account" "this" {
     account_id   = "srv-${local.service}"
-    display_name = "srv_crowemi"
+    display_name = "srv-${local.service}-${var.env}"
     description  = "A service account for ${local.service}"
 }
 
-resource "google_cloud_run_v2_service" "crowemi" {
-    name     = local.service
-    location = local.region
-    ingress = "INGRESS_TRAFFIC_ALL"
-    launch_stage = "BETA"
-    template {
-        containers {
-            image = "us-west1-docker.pkg.dev/${local.project}/crowemi-io/${local.service}:${var.docker_image_tag}"
-        }
-        service_account = google_service_account.service_account.email
-        vpc_access{
-            network_interfaces {
-                network = "crowemi-io-network" # TODO: ref data
-                subnetwork = "crowemi-io-subnet-01" # TODO: ref data
-            }
-            egress = "ALL_TRAFFIC"
-        }
-    }
-}
+# resource "google_cloud_run_v2_service" "crowemi" {
+#     name     = local.service
+#     location = local.region
+#     ingress = "INGRESS_TRAFFIC_ALL"
+#     launch_stage = "BETA"
+#     template {
+#         containers {
+#             image = "us-west1-docker.pkg.dev/${local.project}/crowemi-io/${local.service}:${var.docker_image_tag}"
+#         }
+#         service_account = google_service_account.service_account.email
+#         vpc_access{
+#             network_interfaces {
+#                 network = "crowemi-io-network" # TODO: ref data
+#                 subnetwork = "crowemi-io-subnet-01" # TODO: ref data
+#             }
+#             egress = "ALL_TRAFFIC"
+#         }
+#     }
+# }
 data "google_iam_policy" "noauth" {
     binding {
         role = "roles/run.invoker"
@@ -37,23 +37,23 @@ data "google_iam_policy" "noauth" {
     }
 }
 
-resource "google_cloud_run_service_iam_policy" "noauth" {
-    location    = google_cloud_run_v2_service.crowemi.location
-    project     = google_cloud_run_v2_service.crowemi.project
-    service     = google_cloud_run_v2_service.crowemi.name
+# resource "google_cloud_run_service_iam_policy" "noauth" {
+#     location    = google_cloud_run_v2_service.crowemi.location
+#     project     = google_cloud_run_v2_service.crowemi.project
+#     service     = google_cloud_run_v2_service.crowemi.name
 
-    policy_data = data.google_iam_policy.noauth.policy_data
-}
+#     policy_data = data.google_iam_policy.noauth.policy_data
+# }
 
-resource "google_cloud_run_domain_mapping" "crowemi" {
-    location = "us-west1"
-    name     = "crowemi.com"
+# resource "google_cloud_run_domain_mapping" "crowemi" {
+#     location = "us-west1"
+#     name     = "crowemi.com"
 
-    metadata {
-        namespace = local.project
-    }
+#     metadata {
+#         namespace = local.project
+#     }
 
-    spec {
-        route_name = google_cloud_run_v2_service.crowemi.name
-    }
-}
+#     spec {
+#         route_name = google_cloud_run_v2_service.crowemi.name
+#     }
+# }
