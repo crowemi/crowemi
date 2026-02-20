7926f9c813083a816f97e6b68f18a9138aedb60e
Andy Crowe <crowemi@hotmail.com>
2026-02-19 21:02:45 -0800

    remove old iam/policy

--- infra/main.tf
+++ infra/main.tf
@@ -28,22 +28,6 @@ resource "google_cloud_run_v2_service" "this" {
         }
     }
 }
-data "google_iam_policy" "noauth" {
-    binding {
-        role = "roles/run.invoker"
-        members = [
-            "allUsers",
-        ]
-    }
-}
-
-resource "google_cloud_run_service_iam_policy" "noauth" {
-    location    = google_cloud_run_v2_service.this.location
-    project     = google_cloud_run_v2_service.this.project
-    service     = google_cloud_run_v2_service.this.name
-
-    policy_data = data.google_iam_policy.noauth.policy_data
-}
 
 # resource "google_cloud_run_domain_mapping" "crowemi" {
 #     location = "us-west1"
--- infra/policy.tf
+++ infra/policy.tf
@@ -1,5 +1,5 @@
 resource "google_project_organization_policy" "allow_all_members" {
-  project    = "crowemi-dev"
+  project    = local.project
   constraint = "iam.allowedPolicyMemberDomains"
 
   restore_policy {
